e06f281c3ba254cb537d9dbc669a1acc331dd01b
maikonadvogadomg-alt <maikon.advogado.mg@gmail.com>
2026-05-09 14:52:33 -0300

    Enviado pelo SK Code Editor - 09/05/2026

---
+++ apk-builder-android/android/app/src/main/java/br/com/maikoncaldeira/apkbuilder/MainActivity.java
@@ -0,0 +1,8 @@
+package br.com.maikoncaldeira.apkbuilder;
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+public class MainActivity extends BridgeActivity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
